ddc68b331146647376a68deb0582ce9a8b8992b0
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-04-13 11:16:40 -0300

    adicionar

--- encapsulamento/ContaCorrente.java
+++ encapsulamento/ContaCorrente.java
@@ -1,7 +1,31 @@
 package encapsulamento;
 
 public class ContaCorrente {
-    public int numero;
-    public double saldo;
+    private int numero;
+    private double saldo;
+
+    private ContaCorrente(){}
+
+    public ContaCorrente(int numeroConta){
+        this.numero = numeroConta;
+        this.saldo = 0;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int numeroConta){
+        //auditoria, pois forcei o usuario a usar esse metodo, passar pelo setNumero
+
+        this.numero = numeroConta;
+        this.saldo = 0;
+    }
    
 }
+
+
--- encapsulamento/Sistema.java
+++ encapsulamento/Sistema.java
@@ -1,10 +1,12 @@
 package encapsulamento;
 
 public class Sistema {
-    public static void main(String[] args) {
-        ContaCorrente c1 = new ContaCorrente();
-        ContaCorrente c2 = new ContaCorrente();
-        c1.saldo = 100;
+    public static void main(String[] args) {  /*chama o metodo contrutor, proteger o metodo do contrutor vazio*/
+        ContaCorrente c1 = new ContaCorrente(123); 
+        ContaCorrente c2 = new ContaCorrente(124);
+        c2.setNumero(0); = 125;
+
+        c1.getSaldo();
   
     }
 }
