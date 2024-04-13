1149617f2757dc2d00a0403c7ddea8a8eae8b678
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-04-13 11:17:43 -0300

    adicionar

--- encapsulamento/Sistema.java
+++ encapsulamento/Sistema.java
@@ -4,7 +4,7 @@ public class Sistema {
     public static void main(String[] args) {  /*chama o metodo contrutor, proteger o metodo do contrutor vazio*/
         ContaCorrente c1 = new ContaCorrente(123); 
         ContaCorrente c2 = new ContaCorrente(124);
-        c2.setNumero(0); = 125;
+        //c2.numero() = 125;
 
         c1.getSaldo();
   
