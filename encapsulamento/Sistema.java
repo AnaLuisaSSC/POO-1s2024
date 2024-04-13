525a8593f87dedd03272cec87e955a22c162fca4
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-04-13 10:45:12 -0300

    adicionar

---
+++ encapsulamento/Sistema.java
@@ -0,0 +1,11 @@
+package encapsulamento;
+
+public class Sistema {
+    public static void main(String[] args) {
+        ContaCorrente c1 = new ContaCorrente();
+        ContaCorrente c2 = new ContaCorrente();
+        c1.saldo = 100;
+  
+    }
+}
+ 
